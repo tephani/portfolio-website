a53b65563f9f8e7479bd872e7047385ef88a9d4a
Stephanie Palero <st.palero@gmail.com>
2024-06-16 08:30:41 +0800

    Updated changes.

--- src/app/components/Certifications.jsx
+++ src/app/components/Certifications.jsx
@@ -3,20 +3,32 @@ import Link from "next/link";
 
 const Certifications = () => {
   const certifications = [
-    { link: "https://www.credly.com/badges/6f93e856-6420-480c-ad04-31cb7aa57f7b/public_url", image: "/images/certifications/java-database.png" },
-    { link: "https://www.credly.com/badges/ad1efd13-bb29-477c-afd9-de8eede10800/linked_in_profile", image: "/images/certifications/java-specialist.png" },
-    { link: "https://www.datacamp.com/skill-verification/DL0026821844411", image: "/images/certifications/data-literacy.png" },
-    { link: "https://www.datacamp.com/skill-verification/AIF0028353629564", image: "/images/certifications/ai-fundamentals.png" },
+    {
+      link: "https://www.credly.com/badges/6f93e856-6420-480c-ad04-31cb7aa57f7b/public_url",
+      image: "/images/certifications/java-database.png",
+    },
+    {
+      link: "https://www.credly.com/badges/ad1efd13-bb29-477c-afd9-de8eede10800/linked_in_profile",
+      image: "/images/certifications/java-specialist.png",
+    },
+    {
+      link: "https://www.datacamp.com/skill-verification/DL0026821844411",
+      image: "/images/certifications/data-literacy.png",
+    },
+    {
+      link: "https://www.datacamp.com/skill-verification/AIF0028353629564",
+      image: "/images/certifications/ai-fundamentals.png",
+    },
   ];
 
   return (
     <>
-      <h2 className="text-4xl font-bold text-white mt-10 mb-2 text-center">
-        Certifications
-      </h2>
-      <div className="bg-white rounded-lg border border-[#fffdde] mt-1">
+      <div className=" rounded-lg border border-[#fffdde] mt-1 pb-6 pl-4 pr-4">
         <section className="text-white">
-          <div className="grid grid-cols-4 gap-4 justify-center py-4 px-2 md:px-4">
+          <h2 className="text-4xl font-bold text-white mt-10 mb-4 text-center">
+            Certifications
+          </h2>
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 justify-center py-4 px-2 md:px-4">
             {certifications.map((cert, index) => (
               <div key={index} className="flex items-center justify-center">
                 <Link href={cert.link}>
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,127 +1,127 @@
-"use client";
-import React, { useState } from "react";
+// "use client";
+// import React, { useState } from "react";
 
-const EmailSection = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
+// const EmailSection = () => {
+//   const [emailSubmitted, setEmailSubmitted] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    };
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const data = {
+//       email: e.target.email.value,
+//       subject: e.target.subject.value,
+//       message: e.target.message.value,
+//     };
+//     const JSONdata = JSON.stringify(data);
+//     const endpoint = "/api/send";
+//     const options = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSONdata,
+//     };
 
-    try {
-      const response = await fetch(endpoint, options);
+//     try {
+//       const response = await fetch(endpoint, options);
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
 
-      const resData = await response.json();
+//       const resData = await response.json();
 
-      if (response.status === 200) {
-        console.log("Message sent.");
-        setEmailSubmitted(true);
-        setErrorMessage(null);
-      }
-    } catch (error) {
-      console.error("Error occurred while sending message:", error);
-      setErrorMessage("Failed to send message. Please try again later.");
-    }
-  };
+//       if (response.status === 200) {
+//         console.log("Message sent.");
+//         setEmailSubmitted(true);
+//         setErrorMessage(null);
+//       }
+//     } catch (error) {
+//       console.error("Error occurred while sending message:", error);
+//       setErrorMessage("Failed to send message. Please try again later.");
+//     }
+//   };
 
-  return (
-    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
-      <div>
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
-          I'm currently looking for new opportunities, my inbox is always open.
-          Whether you have questions or just want to say hi, I'll try my best to
-          get back to you!
-        </p>
-      </div>
-      <div>
-        <form className="flex flex-col" onSubmit={handleSubmit}>
-          <div className="mb-6">
-            {" "}
-            <label
-              htmlFor="email"
-              className="text-white block mb-2 text-sm font-medium"
-            >
-              Your Email
-            </label>
-            <input
-              name="email"
-              type="email"
-              id="email"
-              required
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="youremail@gmail.com"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="subject"
-              className="text-white block text-sm mb-2 font-medium"
-            >
-              Subject
-            </label>
-            <input
-              name="subject"
-              type="text"
-              id="subject"
-              required
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="Say something..."
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="message"
-              className="text-white block text-sm mb-2 font-medium"
-            >
-              Message
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="Let's talk about. . ."
-            ></textarea>
-          </div>
-          <div className="text-right">
-            <button
-              type="submit"
-              className="bg-gradient-to-br from-red-500 via-red-500 to-pink-500 hover:bg-slate-200 text-white font-medium py-2.5 px-5 rounded-lg w-full"
-            >
-              Send Message
-            </button>
-            {emailSubmitted && (
-              <p className="text-green-500 text-sm mt-2">
-                Email sent successfully!
-              </p>
-            )}
-            {errorMessage && (
-              <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
-            )}
-          </div>
-        </form>
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
+//       <div>
+//         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+//         <p className="text-[#ADB7BE] mb-4 max-w-md">
+//           {" "}
+//           I'm currently looking for new opportunities, my inbox is always open.
+//           Whether you have questions or just want to say hi, I'll try my best to
+//           get back to you!
+//         </p>
+//       </div>
+//       <div>
+//         <form className="flex flex-col" onSubmit={handleSubmit}>
+//           <div className="mb-6">
+//             {" "}
+//             <label
+//               htmlFor="email"
+//               className="text-white block mb-2 text-sm font-medium"
+//             >
+//               Your Email
+//             </label>
+//             <input
+//               name="email"
+//               type="email"
+//               id="email"
+//               required
+//               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//               placeholder="youremail@gmail.com"
+//             />
+//           </div>
+//           <div className="mb-6">
+//             <label
+//               htmlFor="subject"
+//               className="text-white block text-sm mb-2 font-medium"
+//             >
+//               Subject
+//             </label>
+//             <input
+//               name="subject"
+//               type="text"
+//               id="subject"
+//               required
+//               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//               placeholder="Say something..."
+//             />
+//           </div>
+//           <div className="mb-6">
+//             <label
+//               htmlFor="message"
+//               className="text-white block text-sm mb-2 font-medium"
+//             >
+//               Message
+//             </label>
+//             <textarea
+//               name="message"
+//               id="message"
+//               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//               placeholder="Let's talk about. . ."
+//             ></textarea>
+//           </div>
+//           <div className="text-right">
+//             <button
+//               type="submit"
+//               className="bg-gradient-to-br from-red-500 via-red-500 to-pink-500 hover:bg-slate-200 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+//             >
+//               Send Message
+//             </button>
+//             {emailSubmitted && (
+//               <p className="text-green-500 text-sm mt-2">
+//                 Email sent successfully!
+//               </p>
+//             )}
+//             {errorMessage && (
+//               <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
+//             )}
+//           </div>
+//         </form>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default EmailSection;
+// export default EmailSection;
